f6496fe3a7cbc47fb2c76b899d374a4a9d572478
dunamismax <dunamismax@tutamail.com>
2025-06-18 11:08:55 -0400

    Updates

---
+++ _playground/main.go
@@ -0,0 +1,7 @@
+package main // Declares the package as 'main', which is required for executable programs in Go.
+
+import "fmt" // Imports the "fmt" package, which provides functions for formatted I/O (like printing to the console).
+
+func main() { // Defines the 'main' function, the entry point for every executable Go program.
+	fmt.Println("Hello, World!") // Uses the Println function from the "fmt" package to print the string "Hello, World!" followed by a newline character to the console.
+}
